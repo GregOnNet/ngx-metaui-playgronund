24c45ccbc77787ec00c9b39c093dc02310493290
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-01-23 06:34:08 +0100

    chore: instal meta-ui for prime-ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MetaConfig, MetaUIRulesModule } from '@ngx-metaui/rules';
+import * as userRules from './rules/user-rules';
+import { PrimeNgRulesModule } from '@ngx-metaui/primeng-rules';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MetaUIRulesModule.forRoot({}),
+    PrimeNgRulesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(private config: MetaConfig) {
+    
+   // mandatory - you need to register app defined rules and types since there is no
+   // introspection in js
+
+    const rules: any[] = config.get('metaui.rules.user-rules') || [];
+    rules.push(userRules);
+    config.set('metaui.rules.user-rules', rules);
+
+  }
+}
---
+++ src/app/rules/user-rules.ts
@@ -0,0 +1,4 @@
+/**
+ * Content of this file is automatically populated by oss compile
+ */
+export {DummyRule} from './ts/Dummy.oss';
